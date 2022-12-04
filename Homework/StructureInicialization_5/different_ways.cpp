f05c6a14a746326f269e02889886289165af4598
ArinaTs <GnezdoBobra@yandex.ru>
2022-12-04 09:47:28 +0300

    Переименование

--- Homework/StructureInicialization_5/different_ways.cpp
+++ Homework/StructureInicialization_5/different_ways.cpp
@@ -9,7 +9,7 @@ struct Time {
 
 //4 примера функций заполнения структур данных
 //Пример 1.
-struct Time createTime1 (int h, int m, int s) {
+struct Time createTime1(int h, int m, int s) {
 	struct Time lesson;
 	lesson.hours = h;
 	lesson.minutes = m;
@@ -18,7 +18,7 @@ struct Time createTime1 (int h, int m, int s) {
 }
 
 //Пример 2.
-void createTime2 (struct Time* lesson) {
+void createTime2(struct Time* lesson) {
 	int hours, minutes, seconds;
 
 	std::cout << "Input hours: ";
@@ -28,13 +28,13 @@ void createTime2 (struct Time* lesson) {
 	std::cout << "Input seconds: ";
 	std::cin >> seconds;
 
-	lesson -> hours = hours;
-	lesson -> minutes = minutes;
-	lesson -> seconds = seconds;
+	lesson->hours = hours;
+	lesson->minutes = minutes;
+	lesson->seconds = seconds;
 }
 
 //Пример 3.
-struct Time createTime3 (struct Time lesson) {
+struct Time createTime3(struct Time lesson) {
 	struct Time next_lesson;
 	next_lesson.hours = lesson.hours;
 	next_lesson.minutes = lesson.minutes;
@@ -42,7 +42,7 @@ struct Time createTime3 (struct Time lesson) {
 }
 
 //Пример 4.
-void createTime4 (struct Time* wakeUp) {
+void createTime4(struct Time* wakeUp) {
 	std::cout << "Input hours: ";
 	std::cin >> ((*wakeUp).hours);
 	std::cout << "Input minutes: ";
@@ -50,7 +50,3 @@ void createTime4 (struct Time* wakeUp) {
 	std::cout << "Input seconds: ";
 	std::cin >> ((*wakeUp).seconds);
 }
-
-
-
-
